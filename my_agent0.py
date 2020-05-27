1fc650c47f2bfc7b7e5ab09f212ac76762592edd
bafurtado <furtadobb@gmail.com>
2020-05-27 19:59:56 -0300

    agent0

---
+++ my_agent0.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+if __name__ == '__main__':
+    maria = turtle.Turtle()
+    maria.speed(1)
+    maria.fd(100)
+    maria.rt(90)
+    maria.fd(100)
+    maria.penup()
+    maria.fd(100)
+    maria.color('red')
+    maria.pendown()
+    maria.fd(100)
+    maria.rt(90)
+    maria.fd(100)
+    turtle.mainloop()
