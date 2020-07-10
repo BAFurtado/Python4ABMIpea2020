b210bace3657fe336b73628460b35392f763469d
bafurtado <furtadobb@gmail.com>
2020-07-10 12:57:25 -0300

    newbie_bot

--- robotgame/REAME.md
+++ robotgame/REAME.md
@@ -4,7 +4,7 @@ GitHub repository: `https://github.com/RobotGame/rgkit`
 Rules of the game: `https://github.com/RobotGame/rgkit/blob/master/docs/game_rules.rst`
 0. `mkdir robotgame` `cd robotgame`
 1. Create a virtual environment with python 2.7: `virtualenv -p python2.7 robot`
-2. Change environtment `source robot/bin/activate`
+2. Change environment `source robot/bin/activate`
 3. `pip install rgkit`
 4. `sudo apt-get install python-tk`
 5. Open source bots `https://github.com/mpeterv/robotgame-bots`
--- robotgame/my_bots/newbie.py
+++ robotgame/my_bots/newbie.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """
 
 # ['move', (x, y)]
@@ -19,21 +21,58 @@ rg.locs_around(self.location, filter_out=('invalid', 'obstacle'))
 method rg.wdist(loc1, loc2)
 
 """
+import rg
 
 
-def process_game(game):
+def process_game(game, id):
     self = list()
     others = list()
     for key in game['robots'].keys():
-        if game['robots'][key]['player_id'] == 0:
+        if game['robots'][key]['player_id'] == id:
             self.append(key)
         else:
             others.append(key)
     return self, others
 
 
+def test_location(loc):
+    if loc not in rg.settings.obstacles and loc not in rg.settings.spawn_coords:
+        return True
+    return False
+
+
 class Robot:
 
+    def __init__(self):
+        pass
+
     def act(self, game):
-        # TODO: quais as estratégias a serem considerar
+        us, them = process_game(game, self.player_id)
+        x = int(sum([x[0] for x in us])/len(us))
+        y = int(sum([y[1] for y in us])/len(us))
+        if self.location == (x, y):
+            return ['guard']
+        enemies_around = list()
+        friends_around = list()
+        for each in them:
+            if rg.dist(self.location, each) == 1:
+                enemies_around.append(each)
+        for each in us:
+            if rg.dist(self.location, each) == 1:
+                friends_around.append(each)
+        if len(friends_around) >= 3:
+            return ['guard']
+        distance_border = min([rg.wdist(place, self.location) for place in rg.settings.spawn_coords])
+        if len(friends_around) >= 2 and distance_border > 3:
+            return ['guard']
+        if len(enemies_around) >= 2:
+            return ['suicide']
+        if self.hp < 8 and enemies_around:
+            return ['suicide']
+        for each in them:
+            if rg.dist(self.location, each) == 1:
+                if test_location(each):
+                    return ['attack', each]
+        if test_location((x, y)):
+            return ['move', rg.toward(self.location, (x, y))]
         return ['guard']
