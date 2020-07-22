ab6f0ccd015f43c4717dfca62eabd9ae7fe0e4de
bafurtado <furtadobb@gmail.com>
2020-07-21 21:09:51 -0300

    graficos 0, 2, 3

--- graficos2.py
+++ graficos2.py
@@ -9,10 +9,16 @@ fig1 = plt.figure()  # an empty figure with no axes
 x = np.linspace(0, 2, 100)
 
 # plt.plot -- linha x por x -- parametros: 1o. é o x, o 2o. é o y
+y = x
 plt.plot(x, x, label='linear', color='black')
-plt.plot(x, x**2, label='quadratic', color='pink')
-plt.plot(x, x**3, label='cubic', color='brown')
-#
+y = x ** 2
+plt.plot(x, y, label='quadratic', color='pink')
+y = x ** 3
+plt.plot(x, y, label='cubic', color='brown')
+y = np.ones(100) * 4
+plt.plot(x, y, label='média', color='blue')
+plt.plot(x, x ** .5, label='raiz', color='green')
+
 plt.xlabel('x label')
 plt.ylabel('y label')
 #
@@ -21,11 +27,11 @@ plt.legend(frameon=False)
 plt.show()
 
 # Outro gráfico. Esse é o caso padrão, cria-se a figura e as modificações se dão no ax/axes
-# #
-x = np.arange (0, 10,0.2)
-y = np.cos(x)
-z = np.sin(x)
-fig, ax = plt.subplots()
-ax.plot(x, y, color='red')
-ax.plot(x, z, color='green')
-plt.show()
+# # #
+# x = np.arange (0, 10,0.2)
+# y = np.cos(x)
+# z = np.sin(x)
+# fig, ax = plt.subplots()
+# ax.plot(x, y, color='red')
+# ax.plot(x, z, color='green')
+# plt.show()
